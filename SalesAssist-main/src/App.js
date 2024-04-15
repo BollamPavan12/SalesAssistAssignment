4c45ce769c92f349e56df7571062ee4cb9752791
Bucky Roberts <bollamBrothers955@gmail.com>
2024-04-15 19:41:53 +0530

    first commit

---
+++ SalesAssist-main/src/App.js
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+import Home from './components/Home'
+import './App.css';
+
+class App extends Component {
+  render(){
+    return(
+      <div>
+        <Home/>
+      </div>
+    )
+  }
+}
+
+export default App;
